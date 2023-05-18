fbe7964315c991926d6b9faccd4efad6b9956d98
Soni Nisha <soni.nishaa24@gmail.com>
2023-05-18 14:38:43 +0530

    project1

---
+++ src/main/java/com/restapi/springboot/mapper/AutoUserMapper.java
@@ -0,0 +1,16 @@
+package com.restapi.springboot.mapper;
+
+import com.restapi.springboot.dto.UserDto;
+import com.restapi.springboot.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AutoUserMapper {
+
+    AutoUserMapper MAPPER = Mappers.getMapper(AutoUserMapper.class);
+
+    UserDto mapToUserDto(User user);
+
+    User mapToUser(UserDto userDto);
+}
---
+++ src/main/java/com/restapi/springboot/service/UserService.java
@@ -0,0 +1,18 @@
+package com.restapi.springboot.service;
+
+import com.restapi.springboot.dto.UserDto;
+import com.restapi.springboot.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    UserDto createUser(UserDto user);
+
+    UserDto getUserById(Long userId);
+
+    List<UserDto> getAllUsers();
+
+    UserDto updateUser(UserDto user);
+
+    void deleteUser(Long userId);
+}
